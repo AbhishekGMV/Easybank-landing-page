f6d75e43ea1fefbc9c930b34a98c5000a04232db
Abhishek Gowda M V <abhishek.mvg@gmail.com>
2022-03-20 22:41:51 +0530

    Bug fix - removed scrolling in menu
    Removed scrolling when mobile menu is open

--- app/js/script.js
+++ app/js/script.js
@@ -3,11 +3,14 @@ const headerMenu =document.querySelector(".header__menu");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeItem = document.querySelector(".has-fade");
+const body = document.querySelector('body');
+
 
 burgerMenu.addEventListener("click", () => {
-    if(header.classList.contains("open")) {
+    if(header.classList.contains("open")) { //close
         header.classList.remove("open");
-        
+
+        body.classList.remove("noscroll");
         //mobile menu
         fadeItem.classList.add("has-fade");
 
@@ -16,6 +19,7 @@ burgerMenu.addEventListener("click", () => {
         overlay.classList.add("fade-out");
     } else {
         header.classList.add("open");
+        body.classList.add("noscroll");
         fadeItem.classList.remove("has-fade");
         overlay.classList.remove("fade-out");
         overlay.classList.add("fade-in");
