ad47986a7f25c81dc5324d4175c3d76d613a6523
Abhishek Gowda M V <abhishek.mvg@gmail.com>
2022-03-20 18:02:49 +0530

    Overlay and burger menu animations

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+const burgerMenu= document.querySelector(".header__toggle");
+const headerMenu =document.querySelector(".header__menu");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeItem = document.querySelector(".has-fade");
+
+burgerMenu.addEventListener("click", () => {
+    if(header.classList.contains("open")) {
+        header.classList.remove("open");
+        
+        //mobile menu
+        fadeItem.classList.add("has-fade");
+
+        //overlay
+        overlay.classList.remove("fade-in");
+        overlay.classList.add("fade-out");
+    } else {
+        header.classList.add("open");
+        fadeItem.classList.remove("has-fade");
+        overlay.classList.remove("fade-out");
+        overlay.classList.add("fade-in");
+    }
+});
