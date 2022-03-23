a71017a31baae68a0f9f3775ebd27788752c36f0
Abhishek gowda M V <abhishek.mvg@gmail.com>
2022-03-23 13:03:19 +0530

    Close nav modal on pressing esc key

--- app/js/script.js
+++ app/js/script.js
@@ -1,31 +1,38 @@
-const burgerMenu= document.querySelector(".header__toggle");
-const headerMenu =document.querySelector(".header__menu");
+const burgerMenu = document.querySelector(".header__toggle");
+const headerMenu = document.querySelector(".header__menu");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
-const body = document.querySelector('body');
-
+const body = document.querySelector("body");
 
 burgerMenu.addEventListener("click", () => {
-    if(header.classList.contains("open")) { //close
-        header.classList.remove("open");
-
-        body.classList.remove("noscroll");
-        //mobile menu
-        fadeElems.forEach(function(element){
-            element.classList.remove('fade-in');
-            element.classList.add('fade-out');
-          });
-
-        //overlay
-        overlay.classList.remove("fade-in");
-        overlay.classList.add("fade-out");
-    } else {
-        header.classList.add("open");
-        body.classList.add("noscroll");
-        fadeElems.forEach(function(element){
-            element.classList.remove('fade-out');
-            element.classList.add('fade-in');
-        });
-    }
+  if (header.classList.contains("open")) {
+    closeModal();
+  } else {
+    openModal();
+  }
 });
+
+document.addEventListener("keyup", (event) => {
+  if (event.keyCode == 27 && header.classList.contains("open")) {
+    closeModal();
+  }
+});
+
+const closeModal = () => {
+  header.classList.remove("open");
+  body.classList.remove("noscroll");
+  fadeElems.forEach(function (element) {
+    element.classList.remove("fade-in");
+    element.classList.add("fade-out");
+  });
+};
+
+const openModal = () => {
+  header.classList.add("open");
+  body.classList.add("noscroll");
+  fadeElems.forEach(function (element) {
+    element.classList.remove("fade-out");
+    element.classList.add("fade-in");
+  });
+};
